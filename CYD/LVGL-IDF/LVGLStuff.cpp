c58b570ac8a030fae5f51e82ca895c463693d02b
John Rennie <johnhsrennie@gmail.com>
2026-03-24 09:39:00 +0000

    Add touch to LVGL-IDF

--- CYD/LVGL-IDF/LVGLStuff.cpp
+++ CYD/LVGL-IDF/LVGLStuff.cpp
@@ -15,6 +15,39 @@
 bool InitLVGL(void* panel_handle, int32_t hres, int32_t vres, lv_display_flush_cb_t flush_cb);
 void CreateExampleScreen();
 
+// Touchscreen stuff
+#include <XPT2046_Touchscreen.h>
+
+#define XPT2046_IRQ 36   // T_IRQ
+#define XPT2046_MOSI 32  // T_DIN
+#define XPT2046_MISO 39  // T_OUT
+#define XPT2046_CLK 25   // T_CLK
+#define XPT2046_CS 33    // T_CS
+
+SPIClass touchscreenSPI = SPIClass(VSPI);
+XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+
+// Get the Touchscreen data
+void touchscreen_read(lv_indev_t * indev, lv_indev_data_t * data) {
+  // Checks if Touchscreen was touched, and prints X, Y and Pressure (Z)
+  if(touchscreen.tirqTouched() && touchscreen.touched()) {
+    // Get Touchscreen points
+    TS_Point p = touchscreen.getPoint();
+    // Calibrate Touchscreen points with map function to the correct width and height
+    int x = map(p.x, 200, 3700, 1, 320);
+    int y = map(p.y, 240, 3800, 1, 240);
+
+    data->state = LV_INDEV_STATE_PRESSED;
+
+    // Set the coordinates
+    data->point.x = x;
+    data->point.y = y;
+  }
+  else {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
+
 //----------------------------------------------------------------------
 // Draw some labels
 // Example from the LVGL documentation
@@ -41,9 +74,38 @@ void draw_some_labels(void)
 // Draw some controls
 // Example from Random Nerd Tutorials site
 // https://randomnerdtutorials.com/lvgl-cheap-yellow-display-esp32-2432s028r/
-// The controls don't work since we haven't provided any touch code
 //----------------------------------------------------------------------
-static lv_obj_t* slider_label;
+int btn1_count = 0;
+// Callback that is triggered when btn1 is clicked
+static void event_handler_btn1(lv_event_t * e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  if(code == LV_EVENT_CLICKED) {
+    btn1_count++;
+    LV_LOG_USER("Button clicked %d", (int)btn1_count);
+  }
+}
+
+// Callback that is triggered when btn2 is clicked/toggled
+static void event_handler_btn2(lv_event_t * e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t * obj = (lv_obj_t*) lv_event_get_target(e);
+  if(code == LV_EVENT_VALUE_CHANGED) {
+    LV_UNUSED(obj);
+    LV_LOG_USER("Toggled %s", lv_obj_has_state(obj, LV_STATE_CHECKED) ? "on" : "off");
+  }
+}
+
+static lv_obj_t * slider_label;
+// Callback that prints the current slider value on the TFT display and Serial Monitor for debugging purposes
+static void slider_event_callback(lv_event_t * e) {
+  lv_obj_t * slider = (lv_obj_t*) lv_event_get_target(e);
+  char buf[8];
+  lv_snprintf(buf, sizeof(buf), "%d%%", (int)lv_slider_get_value(slider));
+  lv_label_set_text(slider_label, buf);
+  lv_obj_align_to(slider_label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+  LV_LOG_USER("Slider changed to %d%%", (int)lv_slider_get_value(slider));
+}
+
 void draw_some_controls(void) {
   // Create a text label aligned center on top ("Hello, world!")
   lv_obj_t * text_label = lv_label_create(lv_screen_active());
@@ -56,7 +118,7 @@ void draw_some_controls(void) {
   lv_obj_t * btn_label;
   // Create a Button (btn1)
   lv_obj_t * btn1 = lv_button_create(lv_screen_active());
-  // lv_obj_add_event_cb(btn1, event_handler_btn1, LV_EVENT_ALL, NULL);
+  lv_obj_add_event_cb(btn1, event_handler_btn1, LV_EVENT_ALL, NULL);
   lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -50);
   lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
 
@@ -66,7 +128,7 @@ void draw_some_controls(void) {
 
   // Create a Toggle button (btn2)
   lv_obj_t * btn2 = lv_button_create(lv_screen_active());
-  // lv_obj_add_event_cb(btn2, event_handler_btn2, LV_EVENT_ALL, NULL);
+  lv_obj_add_event_cb(btn2, event_handler_btn2, LV_EVENT_ALL, NULL);
   lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 10);
   lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
   lv_obj_set_height(btn2, LV_SIZE_CONTENT);
@@ -78,7 +140,7 @@ void draw_some_controls(void) {
   // Create a slider aligned in the center bottom of the TFT display
   lv_obj_t * slider = lv_slider_create(lv_screen_active());
   lv_obj_align(slider, LV_ALIGN_CENTER, 0, 60);
-  // lv_obj_add_event_cb(slider, slider_event_callback, LV_EVENT_VALUE_CHANGED, NULL);
+  lv_obj_add_event_cb(slider, slider_event_callback, LV_EVENT_VALUE_CHANGED, NULL);
   lv_slider_set_range(slider, 0, 100);
   lv_obj_set_style_anim_duration(slider, 2000, 0);
 
@@ -162,6 +224,17 @@ bool InitLVGL(void* panel_handle, int32_t hres, int32_t vres, lv_display_flush_c
   // Set the functions we need to provide for LVGL
   lv_tick_set_cb(getMillis);
   lv_display_set_flush_cb(display, flush_cb);
+
+  // Start the SPI for the touchscreen and init the touchscreen
+  touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+  touchscreen.begin(touchscreenSPI);
+  touchscreen.setRotation(3);
+
+  // Initialize an LVGL input device object (Touchscreen)
+  lv_indev_t * indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+  // Set the callback function to read Touchscreen input
+  lv_indev_set_read_cb(indev, touchscreen_read);
 
   // Return indicating success
   return true;
